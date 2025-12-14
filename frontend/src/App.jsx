261fc78f7faf673173e0f2058102386c0c6debe4
Chowa <arfinchowa524@gmail.com>
2025-12-15 00:10:53 +0600

    login and home route added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Registration from './pages/Registration'
+import Home from './pages/Home'
+import Login from './pages/Login'
 
 export const App = () => {
   return (
     <>
     <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
       <Route path="/signup" element={<Registration/>}/>
     </Routes>
     </>
