7461770a91a2909aa9af7d43e05ea423eb06bae8
Chowa <arfinchowa524@gmail.com>
2025-12-16 23:34:51 +0600

    added the react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import Registration from './pages/Registration'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import Registration from './pages/Registration'
+import { Route, Routes } from 'react-router-dom'
+
 
 export const App = () => {
   return (
