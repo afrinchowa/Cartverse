50a454b2f6c356ee4c386c2316c738088ffda577
Chowa <arfinchowa524@gmail.com>
2025-12-17 23:18:09 +0600

    registration page work on

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import Logo from '../assets/logo.png'
 export const Registration = () => {
   return (
-    <div>Registration</div>
+    <div className='w-[100vw] h-[100vh] bg-gradient-to-1 from-[#141414] to[#0c2025] text-[white] flex flex-col items-center justyfy-start'>
+      <div className='w-[100%] h-[80px] flex items-center justify-start px-[30px] gap-[10px]'>
+<img className='w-[40px]' src={Logo} alt="" />
+<h1></h1>
+      </div>
+      Registration</div>
   )
 }
 export default Registration
