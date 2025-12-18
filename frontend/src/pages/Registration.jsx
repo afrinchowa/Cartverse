5983d0a693f9628fbe916d2931c8113d3ffda3b4
Chowa <arfinchowa524@gmail.com>
2025-12-18 23:49:45 +0600

    cartverse name added to the registration

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import Logo from '../assets/logo.png'
+import React from "react";
+import Logo from "../assets/logo.png";
 export const Registration = () => {
   return (
-    <div className='w-[100vw] h-[100vh] bg-gradient-to-1 from-[#141414] to[#0c2025] text-[white] flex flex-col items-center justyfy-start'>
-      <div className='w-[100%] h-[80px] flex items-center justify-start px-[30px] gap-[10px]'>
-<img className='w-[40px]' src={Logo} alt="" />
-<h1></h1>
+    <div className="w-[100vw] h-[100vh] bg-gradient-to-1 from-[#141414] to[#0c2025] text-[white] flex flex-col items-center justyfy-start">
+      <div className="w-[100%] h-[80px] flex items-center justify-start px-[30px] gap-[10px]">
+        <img className="w-[40px]" src={Logo} alt="" />
+        <h1>CartVerse</h1>
       </div>
-      Registration</div>
-  )
-}
-export default Registration
+      Registration
+    </div>
+  );
+};
+export default Registration;
