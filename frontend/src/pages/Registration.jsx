693d2c3430e64c9c0a3edea3d7ff15cf97717f04
Chowa <arfinchowa524@gmail.com>
2025-12-19 22:07:01 +0600

    added the navigation

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Logo from "../assets/logo.png";
+import {useNavigate} from "react-router-dom";
 export const Registration = () => {
+  const navigate = useNavigate();
   return (
-    <div className="w-[100vw] h-[100vh] bg-gradient-to-1 from-[#141414] to[#0c2025] text-[white] flex flex-col items-center justyfy-start">
-      <div className="w-[100%] h-[80px] flex items-center justify-start px-[30px] gap-[10px]">
-        <img className="w-[40px]" src={Logo} alt="" />
-        <h1>CartVerse</h1>
+    <div className="w-screen h-screen bg-gradient-to-1 from-[#141414] to[#0c2025] text-[white] flex flex-col items-center justyfy-start">
+      <div
+        className="w-full h-20 flex items-center justify-start px-[30px] gap-2.5 cursor-pointer "
+        onClick={() => navigate("/")}
+      >
+        <img className="w-10" src={Logo} alt="" />
+        <h1 className="text-[22px] text-black font-sans">CartVerse</h1>
       </div>
       Registration
     </div>
