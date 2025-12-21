aa8ba0fc57ef916e9d7a5f7ade34099dd9860fec
Chowa <arfinchowa524@gmail.com>
2025-12-21 23:37:27 +0600

    registration from decorate

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Logo from "../assets/logo.png";
-import {useNavigate} from "react-router-dom";
+import google from "../assets/google.png";
+import { useNavigate } from "react-router-dom";
 export const Registration = () => {
   const navigate = useNavigate();
   return (
@@ -12,7 +13,29 @@ export const Registration = () => {
         <img className="w-10" src={Logo} alt="" />
         <h1 className="text-[22px] text-black font-sans">CartVerse</h1>
       </div>
-      Registration
+      <div className="w-full h-[100px] flex items-center justify-center flex-col gap-2.5">
+        <span className="text-[25px] font-semibold text-black">
+          Registration page
+        </span>
+        <span className="text-[16px] text-gray-400">
+          Welcome to CartVerse,Place your Order
+        </span>
+      </div>
+
+      <div className="max-w-[600px] w-[90%] h-[500px] bg-[#00000025] border border-[#96969635] backdrop-blur-2xl rounded-lg shadow-lg  flex items-center justify-center">
+        {/* Registration form can be added here */}
+        <form action="" className="w-[90%] h-[90%] flex flex-col items-center justify-start gap-5 ">
+          <div className="w-[90%] h-[50px] bg-[#42656cae] rounded-lg  flex  items-center justify-center gap-2.5 py-5 cursor-pointer">
+            <img src={google} alt="" className="w-7" />
+            Registration with Google
+          </div>
+          <div className="w-full h-5 flex items-center justify-center gap[10px] ">
+<div className="w-full ">
+
+</div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
