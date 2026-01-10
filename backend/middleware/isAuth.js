629b39ad3bbcbb3417f038861e13e24af5d9f0aa
Chowa <arfinchowa524@gmail.com>
2026-01-10 23:43:08 +0600

    is auth token added

--- backend/middleware/isAuth.js
+++ backend/middleware/isAuth.js
@@ -1,6 +1,16 @@
+import token from 'jsonwebtoken'
+
 const isAuth = async (req, res, next) => {
     try {
         // Authentication logic here
+        let token = req.headers.authorization;
+        if (!token) {
+            return res.status(401).json({ message: "No token provided" });
+        }
+    let veryfyToken =jwt.verify(token, process.env.JWT_SECRET);
+        req.user = veryfyToken;
+        
+
         next();
     } catch (error) {
         res.status(401).json({ message: "Unauthorized" });
