5e09d972f2be824f0a5d302486165334a2b5b61f
Chowa <arfinchowa524@gmail.com>
2026-01-11 13:20:58 +0600

    is auth error added

--- backend/middleware/isAuth.js
+++ backend/middleware/isAuth.js
@@ -1,20 +1,22 @@
-import token from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
 const isAuth = async (req, res, next) => {
-    try {
-        // Authentication logic here
-        let token = req.headers.authorization;
-        if (!token) {
-            return res.status(401).json({ message: "No token provided" });
-        }
-    let veryfyToken =jwt.verify(token, process.env.JWT_SECRET);
-        req.user = veryfyToken;
-        
-
-        next();
-    } catch (error) {
-        res.status(401).json({ message: "Unauthorized" });
+  try {
+    // Authentication logic here
+    let token = req.headers.authorization;
+    if (!token) {
+      return res.status(401).json({ message: "No token provided" });
     }
+    let veryfyToken = jwt.verify(token, process.env.JWT_SECRET);
+    if (!veryfyToken) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+
+req.userId = veryfyToken.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: `googleLogin error ${error.message}` });
+  }
 };
 
 module.exports = isAuth;
