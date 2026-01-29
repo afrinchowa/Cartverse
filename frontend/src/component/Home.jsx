7117a0c825d0c0b647d69abdf29c0e7f17220f47
Chowa <arfinchowa524@gmail.com>
2026-01-29 22:34:19 +0600

    home added

---
+++ frontend/src/component/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        Home Component
+        <div>
+            This is the home page of the application.
+            
+        </div>
+    </div>
+  )
+}
+
+export default Home
