dd19938d7cc967b04b91c3040a49d6075aa266a0
Chowa <arfinchowa524@gmail.com>
2026-02-09 22:13:09 +0600

    hero added the text herodata

--- frontend/src/component/Hero.jsx
+++ frontend/src/component/Hero.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 const Hero = ({heroData,heroCount,setHeroCount}) => {
   return (
     <div className='w-40% h-full relative '>
-     <div className=''>
+     <div className='absolute text-[#88d933] text-20px md:text-[40px] lg:text-[55px] md:left-[10%] md:top-[90px] lg:top-[130px] left-[10%] top-2.5 '>
+<p>{heroData.text1}</p>
+<p>{heroData.text2}</p>
+<div>
 
+</div>
      </div>
      </div>
   )
