fc5e0d5926030b5bc6ca6ad8cb6cd96597036206
Chowa <arfinchowa524@gmail.com>
2026-02-20 22:30:03 +0600

    admin routes added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Add from './pages/Add'
+import Lists from './pages/Lists'
+import Orders from './pages/Orders'
+import Login from './pages/Login'
 
 function App() {
   return (
     <>
 <Routes>
-    <Route path='/' element></Route>
+    <Route path='/' element={<Home/>}></Route>
+    <Route path='/add' element={<Add/>}></Route>
+    <Route path='/lists' element={<Lists/>}></Route>
+    <Route path='/orders' element={<Orders/>}></Route>
+    <Route path='/login' element={<Login/>}></Route>
 </Routes>
     </>
   )
