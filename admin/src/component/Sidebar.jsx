1dced656d3048a2e5c745cc36fa0d91d1193e90f
Chowa <arfinchowa524@gmail.com>
2026-03-05 23:03:57 +0600

    sidebar added

---
+++ admin/src/component/Sidebar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { IoMdAddCircleOutline } from "react-icons/io";
+function Sidebar() {
+  return (
+    <div className="w-18% min-h-100vh border-r-[1px] py-[60px] fixed left-0 top-0">
+      <div className="flex flex-col gap-4 pt-40px pl-20% text-15px">
+        <div className="flex items-center justify-center md:justify-start gap-3 border border-grey-200 border-r-0 px-3 py-2 cursor-pointer hover:bg-[#2c7b89] ">
+          <IoMdAddCircleOutline className="h-20px w-20px " />
+          <p className="hidden md:block">Add Items</p>
+        </div>
+        <div className="flex items-center justify-center md:justify-start gap-3 border border-grey-200 border-r-0 px-3 py-2 cursor-pointer hover:bg-[#2c7b89] ">
+          <IoMdAddCircleOutline className="h-20px w-20px " />
+          <p className="hidden md:block">Add Items</p>
+        </div>
+        <div className="flex items-center justify-center md:justify-start gap-3 border border-grey-200 border-r-0 px-3 py-2 cursor-pointer hover:bg-[#2c7b89] ">
+          <IoMdAddCircleOutline className="h-20px w-20px " />
+          <p className="hidden md:block">Add Items</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
--- admin/src/pages/Home.jsx
+++ admin/src/pages/Home.jsx
@@ -5,10 +5,11 @@ import Nav from '../component/Nav'
 function Home() {
   return (
     <>
-    <Nav/>
+ 
     <div className='w-full h-screen md:h-[50vh] text-white sm:h-[30vh] bg-gradient-to-br from-[#141414] to-[#0c2025] flex items-center justify-center text-20px md:text-[40px] lg:text-[55px] font-bold tracking-wide relative overflow-hidden'>
       
-      Home
+   <Nav/>
+    <Sidebar/>
       
       </div>
       </>
