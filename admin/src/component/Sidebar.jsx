a050176513625b00782d3de4512796646e2f9db4
Chowa <arfinchowa524@gmail.com>
2026-03-05 23:11:36 +0600

    sidebar  delete items added

--- admin/src/component/Sidebar.jsx
+++ admin/src/component/Sidebar.jsx
@@ -14,7 +14,7 @@ function Sidebar() {
         </div>
         <div className="flex items-center justify-center md:justify-start gap-3 border border-grey-200 border-r-0 px-3 py-2 cursor-pointer hover:bg-[#2c7b89] ">
           <IoMdAddCircleOutline className="h-20px w-20px " />
-          <p className="hidden md:block">Add Items</p>
+          <p className="hidden md:block">Delete Items</p>
         </div>
       </div>
     </div>
