c33b7b2a72fbd7b1372a983e60247f03eeec4637
Chowa <arfinchowa524@gmail.com>
2026-03-06 20:58:18 +0600

    added the list and order

--- admin/src/component/Sidebar.jsx
+++ admin/src/component/Sidebar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { IoMdAddCircleOutline } from "react-icons/io";
+import { CiCircleList } from "react-icons/ci";
+import { TiTickOutline } from "react-icons/ti";
 function Sidebar() {
   return (
     <div className="w-18% min-h-100vh border-r-[1px] py-[60px] fixed left-0 top-0">
@@ -9,12 +11,12 @@ function Sidebar() {
           <p className="hidden md:block">Add Items</p>
         </div>
         <div className="flex items-center justify-center md:justify-start gap-3 border border-grey-200 border-r-0 px-3 py-2 cursor-pointer hover:bg-[#2c7b89] ">
-          <IoMdAddCircleOutline className="h-20px w-20px " />
-          <p className="hidden md:block">Add Items</p>
+          <CiCircleList className="h-20px w-20px " />
+          <p className="hidden md:block">List Items</p>
         </div>
         <div className="flex items-center justify-center md:justify-start gap-3 border border-grey-200 border-r-0 px-3 py-2 cursor-pointer hover:bg-[#2c7b89] ">
-          <IoMdAddCircleOutline className="h-20px w-20px " />
-          <p className="hidden md:block">Delete Items</p>
+          <TiTickOutline className="h-20px w-20px " />
+          <p className="hidden md:block">Orders</p>
         </div>
       </div>
     </div>
