058458c34ee2966f10edec80e011218bc4c6cfd1
Chowa <arfinchowa524@gmail.com>
2026-03-15 23:39:03 +0600

    added the multer

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,9 +1,9 @@
 import multer, { diskStorage } from 'multer'
 let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/')
+    destination: (req, file, cb) => {
+        cb(null, './public')
     },
-    filename: function (req, file, cb) {
+    filename: (req, file, cb) => {
         cb(null, Date.now() + '-' + file.originalname)
     }
 })
