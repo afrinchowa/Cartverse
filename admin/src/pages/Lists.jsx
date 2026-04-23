cf0fef039e17777314ee4e681a5a14aaa618b29e
Chowa <arfinchowa524@gmail.com>
2026-04-24 01:20:57 +0600

    added the list items 2

--- admin/src/pages/Lists.jsx
+++ admin/src/pages/Lists.jsx
@@ -1,52 +1,55 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
 function Lists() {
-  let [list,setList] = useState([])
-  let {serverUrl} = useContext(authDataContext)
- 
-    const fetchList = async()=>{
-      try {
-        let res = await fetch({serverUrl}+"/api/product/list")
-        setList(result.data)
-        let data = await res.json()
-        setList(result.data)
-        console.log(result.data)
-      } catch (error) {        console.log(error)
-      }
+  let [list, setList] = useState([]);
+  let { serverUrl } = useContext(authDataContext);
+
+  const fetchList = async () => {
+    try {
+      let res = await fetch({ serverUrl } + "/api/product/list");
+      setList(result.data);
+      let data = await res.json();
+      setList(result.data);
+      console.log(result.data);
+    } catch (error) {
+      console.log(error);
     }
-useEffect(()=>{
-  fetchList()
-},[])
+  };
+  useEffect(() => {
+    fetchList();
+  }, []);
 
   return (
-    <div className='w-screen min-h-screen bg-gradient-to-1from[#141414]to-[#0c2025] text-white flex items-center justify-center'>
-<Nav/>
-<div className='w-100% h-100% flex items-center justify-start'>
-<Sidebar/>
-<div className='w-[82%] h-full lg:md-[320px] md:ml-57.5 mt-17.5 flex flex-col gap-7.5 overflow-x-hidden py-12.5 ml-25'>
-<div className='w-100 h-12.5 text-[28px] md:text-[40px] mb-[20px] text-white'>
-All listed products
-</div>
-{
-  list?list.length >0 (
-    list.map((item,index)=>(
-      <div className='w-90% md:h-120px h-90px bg-slate-600 rounded-xl flex items-center justify-start gap-5px md:gap-30px p-10px md:px-30px ' key={index}>
-
+    <div className="w-screen min-h-screen bg-gradient-to-1from[#141414]to-[#0c2025] text-white flex items-center justify-center">
+      <Nav />
+      <div className="w-100% h-100% flex items-center justify-start">
+        <Sidebar />
+        <div className="w-[82%] h-full lg:md-[320px] md:ml-57.5 mt-17.5 flex flex-col gap-7.5 overflow-x-hidden py-12.5 ml-25">
+          <div className="w-100 h-12.5 text-[28px] md:text-[40px] mb-[20px] text-white">
+            All listed products
+          </div>
+          {list ? (
+            list.length >
+            0(
+              list.map((item, index) => (
+                <div
+                  className="w-90% md:h-120px h-90px bg-slate-600 rounded-xl flex items-center justify-start gap-5px md:gap-30px p-10px md:px-30px "
+                  key={index}
+                ></div>
+              )),
+            )
+          ) : (
+            <div className="w-full h-[80%] bg-[#1c1c1c] rounded-lg flex items-center justify-center text-[20px] text-gray-400">
+              No products listed yet
+            </div>
+            <div className="w-full h-[80%] bg-[#1c1c1c] rounded-lg flex items-center justify-center text-[20px] text-gray-400">
+              No products listed yet
+            </div>
+          )}
+        </div>
       </div>
-    ))
-  )
-  :
-  (
-   <div className='w-full h-[80%] bg-[#1c1c1c] rounded-lg flex items-center justify-center text-[20px] text-gray-400'>
-No products listed yet
-</div>
-  )
-}
-
-</div>
-</div>
     </div>
-  )
+  );
 }
 
-export default Lists
+export default Lists;
