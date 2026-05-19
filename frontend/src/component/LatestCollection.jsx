e127642c28695f44ea36109f8842b9c029e19d0a
Chowa <arfinchowa524@gmail.com>
2026-05-19 15:23:03 +0600

    l collection added 1

--- frontend/src/component/LatestCollection.jsx
+++ frontend/src/component/LatestCollection.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function LatestCollection() {
   return (
-    <div>LatestCollection</div>
+    <div className='w-full h-[400px] bg-gray-200 flex items-center justify-center'>
+ <h1 className='text-3xl font-bold text-center mt-10 mb-5'>
+      Latest Collection
+    </h1>
+    </div>
+   
   )
 }
 
