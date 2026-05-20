5f722c7f8e13a622acb7e4ff0f3052a6f01f96f4
Chowa <arfinchowa524@gmail.com>
2026-05-20 23:54:06 +0600

    added the product value

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
+import { authDataContext } from './AuthContext';
 export const shopDataContext=createContext();
-function ShopContext() {
+function ShopContext({children}) {
+   let [products,setProducts]=useState([]); 
+   let {serverUrl}=useContext(authDataContext);
+   let currency= "$";
+   let delivery_fee=5;
+   const getProducts = async()=>{
+    try {
+        let result = await axios.get(serverUrl + "/api/product/list");
+        console.log(result.data);
+        setProducts(result.data);
+        
+    } catch (error) {
+        console.error("Error fetching products:", error);
+    }
+   useEffect(()=>{
+     getProducts();
+    },[])
+
+   
+   
+    let value={
+        products,
+        currency,
+        delivery_fee,
+        getProducts
+    }
   return (
     <div>
-        <shopDataContext.Provider value={{}}>
+        <shopDataContext.Provider value={{value}}>
             <LatestCollection/>
             <BestSeller/>
             <Title/>
+            {children}
         </shopDataContext.Provider> 
     </div>
   )
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,21 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import AuthContext from './context/AuthContext.jsx'
-import UserContext from './context/UserContext.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import AuthContext from "./context/AuthContext.jsx";
+import UserContext from "./context/UserContext.jsx";
+import ShopContext from "./context/ShopContext.jsx";
 
-createRoot(document.getElementById('root')).render(
- <BrowserRouter>
- <AuthContext>
-    <UserContext>
- <App />
-    </UserContext>
-
- </AuthContext>
-
- </BrowserRouter>
-    
-  
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AuthContext>
+      <UserContext>
+        <ShopContext>
+          <App />
+        </ShopContext>
+      </UserContext>
+    </AuthContext>
+  </BrowserRouter>,
+);
