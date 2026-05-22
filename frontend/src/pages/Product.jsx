9b0087b7acfc85dc0c345f6cb5cdf726679586b4
Chowa <arfinchowa524@gmail.com>
2026-05-22 19:24:00 +0600

    added the product value 2

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,15 +2,11 @@ import React from 'react'
 
 function Product() {
   return (
-    <div className='w-[100vw] min-h-[100vh] bg-gradient-to-1 from-[#141414] to-[#0c2025]'>
-      Product
+    <div className='w-[100vw] min-h-[100vh] bg-gradient-to-1from-[#141414]to-[#0c2025] flex items-center justify-start flex-col py-[20px]'>
+     
+     
       </div>
-    <div className='w-[100vw] min-h-[100vh] bg-gradient-to-1 from-[#141414] to-[#0c2025]'>
-      Product
-      </div>
-    <div className='w-[100vw] min-h-[100vh] bg-gradient-to-1 from-[#141414] to-[#0c2025]'>
-      Product
-      </div>
+ 
   )
 }
 
