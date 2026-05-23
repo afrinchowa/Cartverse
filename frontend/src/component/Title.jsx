ea0a524759f5c274a906deb16e441048e99fe788
Chowa <arfinchowa524@gmail.com>
2026-05-23 22:50:20 +0600

    added the title

--- frontend/src/component/Title.jsx
+++ frontend/src/component/Title.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-function Title() {
+function Title({text1 ,text2}) {
   return (
-    <div>Title</div>
+    <div className='inline-flex gap-2 items-center text-center mb-3 text-[35px] md:text-[40px]'>
+      <p>{text1}</p>
+      <p>{text2}</p>
+     </div>
   )
 }
 
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import BestSeller from '../component/BestSeller'
+import React from "react";
+import BestSeller from "../component/BestSeller";
+import LatestCollection from "../component/LatestCollection";
 
 function Product() {
   return (
-    <div className='w-[100vw] min-h-[100vh] bg-gradient-to-1from-[#141414]to-[#0c2025] flex items-center justify-start flex-col py-[20px]'>
-     <div className='w-full min-h-[70px] flex items-center'>
-      <BestSeller/>
-     </div>
-     
+    <div className="w-[100vw] min-h-[100vh] bg-gradient-to-1from-[#141414]to-[#0c2025] flex items-center justify-start flex-col py-[20px]">
+      <div className="w-full min-h-[70px] flex items-center">
+        <LatestCollection />
       </div>
- 
-  )
+      <div className="w-full min-h-[70px] flex items-center">
+        <BestSeller />
+      </div>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
