c92ad985759709df0d8f2d2d2fa2197f4aa7e20e
Chowa <arfinchowa524@gmail.com>
2026-05-24 21:33:20 +0600

    add card

--- frontend/src/component/LatestCollection.jsx
+++ frontend/src/component/LatestCollection.jsx
@@ -5,8 +5,11 @@ function LatestCollection() {
   return (
     <div className=''>
 <div className='h-[8%] w-full text-center md:mt-[50px]'>
-<Title text1={LATEST} text2={COLLECTIONS}/>
-<p>Step Into Style New COllection Dropping This Sesason</p>
+<Title text1={"LATEST"} text2={"COLLECTIONS"}/>
+<p>Step Into Style New COllection Dropping This Sesason!</p>
+</div>
+<div className='w-full h-[50%] mt-[30px] flex items-center justify-center flex-wrap gap-[50px]'>
+  {}
 </div>
     </div>
    
