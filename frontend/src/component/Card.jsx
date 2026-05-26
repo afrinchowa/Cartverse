9322001273b0ac272f718658433cf68e7dd4fae9
Chowa <arfinchowa524@gmail.com>
2026-05-26 12:06:27 +0600

    add card 2

--- frontend/src/component/Card.jsx
+++ frontend/src/component/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Card() {
   return (
     <div className='w-[300px] max-w-[90%]
-     h-[400px] bg'>Card</div>
+     h-[400px] bg-[#ffffff0a] backdrop:blur-lg rounded-lg hover:scale-[102%] flex items-start justify-start flex-col p-[10px]cursor-pointer border0[1px] border-[#90909049]'>Card</div>
   )
 }
 
