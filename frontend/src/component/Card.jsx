f814d1658b7610828cb5bf75175e470250532d5c
Chowa <arfinchowa524@gmail.com>
2026-05-30 21:40:46 +0600

    add card 7

--- frontend/src/component/Card.jsx
+++ frontend/src/component/Card.jsx
@@ -1,28 +1,18 @@
 import React from 'react'
 
-function Card() {
+function Card({name,image, id , price}) {
   return (
     <div className='w-[300px] max-w-[90%]
-     h-[400px] bg-[#ffffff0a] backdrop:blur-lg rounded-lg hover:scale-[102%] flex items-start justify-start flex-col p-[10px]cursor-pointer border0[1px] border-[#90909049]'>
-      <div className='w-full h-[200px] mb-[10px] flex items-center justify-center'>
-
-<h1 className='text-[18px] font-bold text-center text-[#ffffff]'>
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.
-</h1>
-
-<div className="w-full h-[100px] mb-[10px] flex items-center justify-center">
-<p className='text-[14px] text-center text-[#ffffff]'>
-
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.
-
-
-</p>
+     h-[400px] bg-[#ffffff0a] backdrop:blur-lg rounded-lg hover:scale-[102%] flex items-start justify-start flex-col p-[10px]cursor-pointer border-[1px] border-[#90909049]'>
+  
+      <img src={image} alt={name} className='w-full h-[80%] object-cover rounded-sm' />
+<div className='text-[#c3f6fa] text-[18px] py-[10px]'>
+{name.length > 20 ? name.slice(0, 20) + '...' : name}
 </div>
-<div className='w-full '>
-
+<div className='text-[#f3f6fa] text-[14px] py-[10px]'>
+  {currency} {price}
 </div>
-        <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="" className='w-full h-[200px] object-cover rounded-lg' />
-      </div>
+
 
 
      </div>
