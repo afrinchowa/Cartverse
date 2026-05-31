3411412268f2a3d99acd5e75e861d3ba933b6d50
Chowa <arfinchowa524@gmail.com>
2026-05-31 22:27:33 +0600

    latest collection added

--- frontend/src/component/LatestCollection.jsx
+++ frontend/src/component/LatestCollection.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import Title from './Title'
-
+import { useContext } from 'react'
+import { shopDataContext } from '../context/shopDataContext'
 function LatestCollection() {
+  let {products} = useContext(shopDataContext)
+  let [latestProducts, setLatestProducts] = useState([])
+  useEffect(() => {
+
+    setLatestProducts(products.slice(0,8));
+  }, [products]);
   return (
     <div className=''>
 <div className='h-[8%] w-full text-center md:mt-[50px]'>
@@ -9,7 +16,11 @@ function LatestCollection() {
 <p>Step Into Style New COllection Dropping This Sesason!</p>
 </div>
 <div className='w-full h-[50%] mt-[30px] flex items-center justify-center flex-wrap gap-[50px]'>
-  {}
+  {
+    latestProducts.map((item,index)=>(
+      <Card key={index} name={item.name} image={item.image1}  id={item._id} price={item.price}/>
+    ))
+  }
 </div>
     </div>
    
