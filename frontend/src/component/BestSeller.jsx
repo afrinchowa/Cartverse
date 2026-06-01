3603c78c7b7419a28bc118a0ea92e53ad0cfafed
Chowa <arfinchowa524@gmail.com>
2026-06-01 23:04:48 +0600

    best seller added 1

--- frontend/src/component/BestSeller.jsx
+++ frontend/src/component/BestSeller.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Title from './Title'
 
 function BestSeller() {
   return (
     <div
-    className="h-[8%] w-full text-center mt-[50px]  text-2xl font-bold text-center my-8"
-    >BestSeller</div>
+    className="h-[8%] w-full text-center mt-[50px]  "
+    >
+      <Title text1={"BEST"} text2={"SELLERS"} />
+      <p>Discover Our Best Sellers - Shop Now and Experience the Difference!</p>
+    </div>
   )
 }
 
