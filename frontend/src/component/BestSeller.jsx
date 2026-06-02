a5c73e92af910587c92fbba7aa59b72672b77a1b
Chowa <arfinchowa524@gmail.com>
2026-06-02 23:36:14 +0600

    best seller added 23

--- frontend/src/component/BestSeller.jsx
+++ frontend/src/component/BestSeller.jsx
@@ -8,6 +8,12 @@ function BestSeller() {
     >
       <Title text1={"BEST"} text2={"SELLERS"} />
       <p>Discover Our Best Sellers - Shop Now and Experience the Difference!</p>
+    
+    <div className="h-[80%] w-full bg-gray-200 mt-[20px]" 
+    
+    >
+        <p className='text-[20px] text-gray-500 mt-[20px]'>Best sellers will be displayed here.</p>
+    </div>
     </div>
   )
 }
