6188afd95cbc525227b0213860c0c8a6783b41f0
Chowa <arfinchowa524@gmail.com>
2026-06-09 03:14:14 +0600

    collection added update categories

--- frontend/src/pages/Collections.jsx
+++ frontend/src/pages/Collections.jsx
@@ -1,18 +1,27 @@
 import { Search, Heart, ShoppingBag, Star } from "lucide-react";
 
 function Collections() {
+  let [showFilter,setShowFilter] = useState(false); 
   return (
     <div className="w-full min-h-screen bg-linear-to-l from-gray-[#141414] to-gray-[#0c2025] flex flex-col items-start md:flex-row justify-start pt-[70px] overflow-x-hidden z-2 ">
       <div className=" md:w-[30vw] lg:w-[20vw] w-[100vw] md:min-h-[100vh] p-[20px] border-r-[1px] border-gray-400 text-[#aaf5fa] lg:fixed flex flex-col items-center justify-center gap-4">
-        <p className="text-[25px] font-semibold flex gap-[5px] items-center justify-start">
-          FILTERS{" "}
+        <p className="text-[25px] font-semibold flex gap-[5px] items-center justify-start" onClick={()=>setShowFilter(prev => !prev)}  >
+          FILTERS{""}
         </p>
-        <div className="border-2px border-[#dedcdc] pl-5 py-3 mt-6 rounded-md bg-slate-600 ">
+        <div className={`border-2px border-[#dedcdc] pl-5 py-3 mt-6 rounded-md bg-slate-600 ${showFilter? "" : hidden}  md:block` } >
           <p className="text-[18px] text-[#f8fafa] font-medium">CATEGORIES</p>
           <div className="w-[230px] h-[120px] flex items-start justify-center gap-[10px] flex-col">
             <p className="flex items-center justify-center gap-[10px] text-[16px] font-light"> <input type="checkbox" value={"Men"} className="w-3 h-3"/> Men</p>
             <p className="flex items-center justify-center gap-[10px] text-[16px] font-light"> <input type="checkbox" value={"Women"} className="w-3 h-3"/> Women</p>
             <p className="flex items-center justify-center gap-[10px] text-[16px] font-light"> <input type="checkbox" value={"Kids"} className="w-3 h-3"/> Kids</p>
+          </div>
+        </div>
+      <div className={`border-2px border-[#dedcdc] pl-5 py-3 mt-6 rounded-md bg-slate-600 ${showFilter? "" : hidden}  md:block` } >
+          <p className="text-[18px] text-[#f8fafa] font-medium">SUB-CATEGORIES</p>
+          <div className="w-[230px] h-[120px] flex items-start justify-center gap-[10px] flex-col">
+            <p className="flex items-center justify-center gap-[10px] text-[16px] font-light"> <input type="checkbox" value={"TopWear"} className="w-3 h-3"/> TopWear</p>
+            <p className="flex items-center justify-center gap-[10px] text-[16px] font-light"> <input type="checkbox" value={"BottomWear"} className="w-3 h-3"/> BottomWear</p>
+            <p className="flex items-center justify-center gap-[10px] text-[16px] font-light"> <input type="checkbox" value={"WinterWear"} className="w-3 h-3"/> WinterWear</p>
           </div>
         </div>
       </div>
