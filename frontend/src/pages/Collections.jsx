6bacc8547e8d9f179963ee58028d0c87e94ffd59
Chowa <arfinchowa524@gmail.com>
2026-06-14 00:04:25 +0600

    collection added react icon 3

--- frontend/src/pages/Collections.jsx
+++ frontend/src/pages/Collections.jsx
@@ -6,14 +6,14 @@ import { FaAngleDown } from "react-icons/fa";
 function Collections() {
   let [showFilter,setShowFilter] = useState(false); 
   return (
-    <div className={`w-full min-h-screen bg-linear-to-l from-gray-[#141414] to-gray-[#0c2025] flex flex-col items-start md:flex-row justify-start pt-[70px] overflow-x-hidden z-2 `}>
-      <div className=" md:w-[30vw] lg:w-[20vw] w-[100vw] md:min-h-[100vh] p-[20px] border-r-[1px] border-gray-400 text-[#aaf5fa] lg:fixed flex flex-col items-center justify-center gap-4">
+    <div className={`md:w-[100vw] lg:w-[20vw] w-full min-h-screen bg-linear-to-l from-gray-[#141414] to-gray-[#0c2025] flex flex-col items-start md:flex-row justify-start pt-[70px] overflow-x-hidden z-2 `}>
+      <div className=" md:w-[30vw] lg:w-[20vw] w-[100vw] md:min-h-[100vh] ${showFilter? "h-[45vh]" : "h-[100vh]"} p-[20px] border-r-[1px] border-gray-400 text-[#aaf5fa] lg:fixed flex flex-col items-center justify-center gap-4">
         <p className="text-[25px] font-semibold flex gap-[5px] items-center justify-start cursor-pointer" onClick={()=>setShowFilter(prev => !prev)}  >
           FILTERS{""}
           {!showFilter && <FaAngleRight className="text-[18px] md:hidden" />}
           {showFilter && <FaAngleDown className="text-[18px] md:hidden" />}
         </p>
-       <div className={`border-2px border-[#dedcdc] pl-5 py-3 mt-6 rounded-md bg-slate-600 ${showFilter? "" : "hidden"}  md:block` } >
+       
         <div className={`border-2px border-[#dedcdc] pl-5 py-3 mt-6 rounded-md bg-slate-600 ${showFilter? "" : "hidden"}  md:block` } >
           <p className="text-[18px] text-[#f8fafa] font-medium">CATEGORIES</p>
           <div className="w-[230px] h-[120px] flex items-start justify-center gap-[10px] flex-col">
