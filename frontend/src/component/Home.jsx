639743428535cd5aed948acf25f82d52a3d10a3d
Chowa <arfinchowa524@gmail.com>
2026-07-30 00:21:18 +0600

    home fixing 1

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -3,16 +3,21 @@ const Background = React.lazy(() => import("./Background"));
 const Hero = React.lazy(() => import("./Hero"));
 import { heroData } from "../data/heroData";
 import { useState } from "react";
+import OurPolicy from "./OurPolicy";
   
 const Home = () => {
   const [heroCount, setHeroCount] = useState(0);
   return (
     <div className="min-h-screen bg-gray-50">
-      <Background />
-      <Hero 
+      <div>
+ <Background />
+   <Hero 
       heroCount={heroCount}
       setHeroCount={setHeroCount}
     heroData={heroData[heroCount]}/>
+      </div>
+     <Product/>
+   <OurPolicy/>
    
     </div>
   );
--- frontend/src/component/OurPolicy.jsx
+++ frontend/src/component/OurPolicy.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function OurPolicy() {
   return (
-    <div>
-        
+    <div className='w-[100vw] h-[100vh] md:h-[70vh] flex items-center justify-start flex-col bg-gradient-to-lfrom-[#141414] to-[#0c2025] gap-[50px] '>
+
     </div>
   )
 }
