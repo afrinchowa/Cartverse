626ad5151822e6b024a53394c4f4583544c21d9c
Chowa <arfinchowa524@gmail.com>
2026-07-31 01:14:01 +0600

    home fixing 1

--- frontend/src/component/OurPolicy.jsx
+++ frontend/src/component/OurPolicy.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 function OurPolicy() {
   return (
     <div className='w-[100vw] h-[100vh] md:h-[70vh] flex items-center justify-start flex-col bg-gradient-to-lfrom-[#141414] to-[#0c2025] gap-[50px] '>
-
+<div className='w-[100%] md:w-[70%] h-[8%] text-center mt-[70px]'>
+<Title text1={"Our"} text2={"Policy"}/>
+<p className='text-blue-100 m-auto text-13px md:text-20px px-10px '>
+ Customer friendly policies - Committed to Your Satisfaction and Safety.
+</p>
+</div>
     </div>
   )
 }
