835cb2b318072768c19ab3b496895f7e5482aac2
Chowa <arfinchowa524@gmail.com>
2026-08-10 22:23:58 +0600

    home fixing 5

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -6,9 +6,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    name: {
+  
+    email: {
       type: String,
       required: true,
+      unique: true,
     },
     email: {
       type: String,
